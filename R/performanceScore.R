ee358a10ca25497e3d4c7aef6f7228dcb620a0bb
pascalhorton <pascal.horton@giub.unibe.ch>
2017-05-09 12:37:34 +0200

    Testing the CRPS

--- R/performanceScore.R
+++ R/performanceScore.R
@@ -23,33 +23,32 @@ CRPS <- function(x, x0, a=0.44, b=0.12) {
   r <- 1:n
   Fx <- (r - a) / (n + b)
   res <- 0
-  # add rectangle on right side if observed value is on the right
+  # Add rectangle on right side if observed value is on the right
   if (x0 > x.s[n]) {
     res <- res + x0 - x.s[n]
   }
-  # add rectangle on the left side if observed value is on the left
+  # Add rectangle on the left side if observed value is on the left
   if (x0 < x.s[1]) {
     res <- res + x.s[1] - x0
   }
   if (n > 1) {
     for (i in 1:(n - 1)) {
-      # if value on left side of observed value
+      # If value on left side of observed value
       if (x.s[i] < x0) {
-        # case 1: next value also on left side of observed value
+        # Case 1: next value also on left side of observed value
         if (x.s[i + 1] <= x0) {
           res <- res + (Fx[i] ^ 2 + Fx[i + 1] ^ 2) * (x.s[i + 1] - x.s[i]) / 2
         }
-        # case 2: x0 before next value
+        # Case 2: x0 before next value
         else {
           F0 <- (Fx[i + 1] - Fx[i]) * (x0 - x.s[i]) / (x.s[i + 1] - x.s[i]) + Fx[i]
           res <- res + (F0 ^ 2 + Fx[i] ^ 2) * (x0 - x.s[i]) / 2
           res <- res + ((F0 - 1) ^ 2 + (Fx[i + 1] - 1) ^ 2) * (x.s[i + 1] - x0) / 2
         }
       }
-      # if value on the right side of observed value
+      # If value on the right side of observed value
       else {
-        res <- res + ((Fx[i] - 1) ^ 2 + (Fx[i + 1] - 1) ^ 2) * (x.s[i + 1] - x.s[i]) /
-          2
+        res <- res + ((Fx[i] - 1) ^ 2 + (Fx[i + 1] - 1) ^ 2) * (x.s[i + 1] - x.s[i]) / 2
       }
     }
   }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(atmoswingRToolbox)
+
+test_check("atmoswingRToolbox")
